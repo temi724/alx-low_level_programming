2624fa2610474b0425a1badf7e7c9bf658f551e8
temi724 <tayodaniel0@gmail.com>
2022-03-16 02:40:56 +0100

    solution files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - prints the string "putchar" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
