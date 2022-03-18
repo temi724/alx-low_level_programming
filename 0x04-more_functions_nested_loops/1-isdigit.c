23bced1b233a49f6408b51b58aef351ee1377f59
temi724 <tayodaniel0@gmail.com>
2022-03-19 00:11:10 +0100

    bulk upload

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,11 @@
-
 /**
- *_isupper - return for uppercase
+ *_isdigit - return 1 or 0 if is digit
  *@c: parameter integer
  *Return: 1 or 0
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
