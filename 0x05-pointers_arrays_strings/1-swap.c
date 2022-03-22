266be16eebc2fe6250e0f95876cb973507ee1655
temi724 <tayodaniel0@gmail.com>
2022-03-22 03:53:40 +0100

    checking in solution files

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ *swap_int - swap form integer a to integer b
+ *@a: pointer integer
+ *@b: pointer integer
+ *Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
